f7c6a497504db8cbbf07d57f36e939ffc9d4a3e1
nemoshi-neko <188776090+nemoshi-neko@users.noreply.github.com>
2025-10-24 21:48:22 +0900

    Refactor calculator button data attributes for consistency

--- script.js
+++ script.js
@@ -1,93 +1,92 @@
-const display = document.getElementById('display');
-const message = document.getElementById('message');
-const buttons = document.querySelector('.buttons');
-let state = {
-    GlobalOperand:0,
-    LetOperand:0,
-    NextOpe:0,
-    NANS:10//N-Adic Number System
-}
-const OPS = { ADD: 1, SUB: 2, MUL: 3, DIV: 4 };
-
-function calc(ope,a,b){
-    switch(ope){
-        case OPS.ADD: return a+b;
-        case OPS.SUB: return a-b;
-        case OPS.MUL: return a*b;
-        case OPS.DIV: {
-            if (b === 0) 
-            message.innerHTML="n/0 is not defined";
-            return a/b;
+class brain{
+    constructor(){
+        this.reset()
+    }
+    reset(){
+        this.a=0;
+        this.b=0;
+        this.op=0;
+        this.base=10;
+        mn.render('');
+    }
+    inputnum(num){
+        this.b = this.base*this.b+num
+        mn.format(this.b);
+    }
+    inputop(op){
+        this.op=op;
+        this.a=this.b;
+        this.b=0;
+    }
+    calc(a,b,op){
+        switch(op){
+            case '+': return a+b;
+            case '-': return a-b;
+            case '*': return a*b;
+            case '/':
+                if(b==0){mn.render("n/0 is not defined");return Infinity}
+                return a/b;
         }
     }
+    set(){
+        if(this.b<2 || this.b>16){mn.render("2<=n<=16!!");return;}
+        if(this.op)return;
+        this.base=this.b;
+        this.b=0;
+        mn.format(this.b);
+    }
+    result() {
+        if(!this.op){mn.format(this.b);return;}
+        this.b = this.calc(this.a, this.b, this.op);
+        mn.format(this.b);
+        this.a = this.b;
+        this.b = 0;
+        this.op = null;
+    }
 }
 
-buttons.addEventListener('click', (event) => {
-    const target = event.target;
-    if(!target.matches('button'))return;
+class SensoryNerve{
+    constructor(){
+        this.message = document.getElementById('message');
+        this.buttons = document.querySelector('.buttons');
 
-    if(target.classList.contains('number')){
-        const numberData = target.dataset.number;
-        console.log(numberData);
-        newNum(Number(numberData));
-        return;
+        this.buttons.addEventListener("click", e => {
+            const btn = e.target;
+            if (!btn.matches("button")) return;
+            const action = btn.dataset.action || btn.classList[0];
+            this.actions[action]?.(btn);
+        });
     }
-    if(target.classList.contains('operator')){
-        console.log(target.dataset.operator);
-        newOperator(target.dataset.operator);
-        return;
+    actions = {
+        number: (b) => {
+            const numberData = parseInt(b.dataset.number,br.base);
+            console.log(numberData);
+            if (isNaN(numberData)) return;
+            br.inputnum(numberData);
+        },
+        operator: (b) =>{
+            const op = b.dataset.operator;
+            console.log(op);
+            br.inputop(op)
+        },
+        calculate: () => br.result(),
+        set: () => br.set(),
+        clear: () => br.reset()
     }
-    if(target.dataset.action === 'calculate'){
-        state.GlobalOperand=calc(state.NextOpe,state.GlobalOperand,state.LetOperand)
-        Show(state.GlobalOperand)
-        return;
-    }
-    if (target.classList.contains('set')) {
-        set();
-        return;
-    }
-    if (target.dataset.action === 'clear') {
-        reset();
-        return;
-    }
-})
-
-function newNum(num){
-    if(!state.LetOperand)state.LetOperand=num;
-    else state.LetOperand=state.NANS*state.LetOperand+num;
-    Show(state.LetOperand);
 }
 
-function set(){
-    if(state.LetOperand<2 || state.LetOperand>16){message.innerHTML="2<=n<=16!!";return;}
-    if(state.NextOpe)return;
-    state.NANS=state.LetOperand;
-    state.LetOperand=0;
-    display.value='('+state.NANS+')'
-}
-
-function newOperator(ope){
-    if(!state.NextOpe)state.GlobalOperand=state.LetOperand;
-    else state.GlobalOperand=calc(state.NextOpe,state.GlobalOperand,state.LetOperand)
-    switch(ope){
-        case '+': state.NextOpe = OPS.ADD; break;
-        case '-': state.NextOpe = OPS.SUB; break;
-        case '*': state.NextOpe = OPS.MUL; break;
-        case '/': state.NextOpe = OPS.DIV; break;
+class MotorNerve{
+    constructor(){
+        this.display = document.getElementById('display');
     }
-    state.LetOperand=0;
+    format(num) {
+        this.render(`(${br.base})` + num.toString(br.base).toUpperCase())
+    }
+    render(s){
+        this.display.value = s;
+    }
 }
 
-function Show(num){
-    display.value='('+state.NANS+')'+num.toString(state.NANS).toUpperCase();
-}
-
-function reset(){
-    state = { 
-        GlobalOperand: 0, 
-        LetOperand: 0, 
-        NextOpe: 0, 
-        NANS: 10
-    };
-    display.value='';
-}
+mn= new MotorNerve
+br= new brain;
+sn= new SensoryNerve;
